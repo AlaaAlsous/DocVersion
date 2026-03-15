b5be595841a69a0f2b3df6e470cf28f71d2fa57d
Alaa Alsous <lawyeralaa@gmail.com>
2026-03-15 17:12:08 +0100

    Configure JWT authentication and authorization

--- DocVersion/Program.cs
+++ DocVersion/Program.cs
@@ -1,5 +1,6 @@
 using System.Text;
 using Microsoft.IdentityModel.Tokens;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 ;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,10 +12,27 @@ var jwtKey = Environment.GetEnvironmentVariable("JWT_KEY")
           ?? throw new InvalidOperationException("JWT key not found in environment variables or configuration.");
 var signingKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey));
 
+builder.Services
+    .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
+    {
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = false,
+            ValidateAudience = false,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
+            IssuerSigningKey = signingKey
+        };
+    });
+
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
