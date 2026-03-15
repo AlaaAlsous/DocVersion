da2c560acf6c4119a233f3f3ef43723f77740c11
Alaa Alsous <lawyeralaa@gmail.com>
2026-03-15 19:11:47 +0100

    Add file metadata model with JSON mappings

---
+++ DocVersion/Models/FileMetadata.cs
@@ -0,0 +1,19 @@
+using System.Text.Json.Serialization;
+
+namespace DocVersion.Models;
+
+public class FileMetadata
+{
+    [JsonPropertyName("created")]
+    public string Created { get; set; } = "";
+    [JsonPropertyName("changed")]
+    public string Changed { get; set; } = "";
+    [JsonPropertyName("file")]
+    public bool IsFile { get; set; }
+    [JsonPropertyName("bytes")]
+    public long Bytes { get; set; }
+    [JsonPropertyName("extension")]
+    public string? Extension { get; set; }
+    [JsonPropertyName("content")]
+    public Dictionary<string, FileMetadata>? Content { get; set; }
+}
