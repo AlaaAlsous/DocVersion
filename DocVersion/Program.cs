9f5b4402032baf6ae5bec0bc86bba614081d2c96
Alaa Alsous <lawyeralaa@gmail.com>
2026-03-15 17:03:11 +0100

    Install Microsoft.AspNetCore.Authentication.JwtBearer for JWT support

--- DocVersion/Program.cs
+++ DocVersion/Program.cs
@@ -1,18 +1,12 @@
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
 
 app.UseHttpsRedirection();
 
@@ -20,4 +14,4 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-app.Run();
+app.Run("http://localhost:3000/");
