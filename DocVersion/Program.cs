67dda379a35172a9e80f387fe25f8961f29f844b
Alaa Alsous <lawyeralaa@gmail.com>
2026-03-22 12:11:00 +0100

    Configure JWT to read token from query for SignalR

--- DocVersion/Program.cs
+++ DocVersion/Program.cs
@@ -47,6 +47,19 @@ builder.Services
             ValidIssuer = jwtIssuer,
             ValidAudience = jwtAudience
         };
+        options.Events = new JwtBearerEvents
+        {
+            OnMessageReceived = context =>
+            {
+                var accessToken = context.Request.Query["access_token"];
+                var path = context.HttpContext.Request.Path;
+                if (!string.IsNullOrEmpty(accessToken) && path.StartsWithSegments("/api/events/signalr"))
+                {
+                    context.Token = accessToken;
+                }
+                return Task.CompletedTask;
+            }
+        };
     });
 
 builder.Services.AddAuthorization();
