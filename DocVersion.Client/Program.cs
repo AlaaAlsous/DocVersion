75ba18ffc2b458b02de7be41ce9eb39a14409d3e
Alaa Alsous <lawyeralaa@gmail.com>
2026-04-03 20:18:52 +0200

    Add URL normalization and console color helper methods.

--- DocVersion.Client/Program.cs
+++ DocVersion.Client/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    public static async Task<int> Main(string[] args)
+    {
+        return 0;
+    }
+
+
+    private static string NormalizeServerUrl(string url)
+    {
+        if (!url.StartsWith("http://", StringComparison.OrdinalIgnoreCase) && !url.StartsWith("https://", StringComparison.OrdinalIgnoreCase))
+        {
+            url = (url.StartsWith("localhost", StringComparison.OrdinalIgnoreCase)
+            || url.StartsWith("localhost:", StringComparison.OrdinalIgnoreCase)
+            || url.StartsWith("localhost/", StringComparison.OrdinalIgnoreCase)
+            || url.StartsWith("127.0.0.1"))
+                  ? $"http://{url}" : $"https://{url}";
+        }
+        return url.TrimEnd('/');
+    }
+
+    private static string MessageColor(string message, ConsoleColor color)
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(message);
+        Console.ResetColor();
+        return message;
+    }
+}
