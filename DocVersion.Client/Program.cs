888888acbe2815db2ca330a2efa8274cc28420ab
Alaa Alsous <lawyeralaa@gmail.com>
2026-04-03 20:20:07 +0200

    Add args check and log working directory.

--- DocVersion.Client/Program.cs
+++ DocVersion.Client/Program.cs
@@ -1,7 +1,24 @@
-﻿class Program
+﻿using System.Net.Http.Json;
+using DocVersion.Core.Helpers;
+using DocVersion.Core.Models;
+using System.Net.Http.Headers;
+
+class Program
 {
     public static async Task<int> Main(string[] args)
     {
+        if (args.Length < 4)
+        {
+            MessageColor("Usage: DocVersion.Client [pull|push] <serverUrl> <username> <password>", ConsoleColor.Red);
+            return 1;
+        }
+        var command = args[0].ToLower();
+        var serverUrl = NormalizeServerUrl(args[1]);
+        var username = args[2];
+        var password = args[3];
+        var cwd = Directory.GetCurrentDirectory();
+
+        MessageColor("Working directory: " + cwd, ConsoleColor.Cyan);
         return 0;
     }
 
