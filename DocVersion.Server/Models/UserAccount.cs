a1269ec2ab743a095baabfde5c6465724972dd98
Alaa Alsous <lawyeralaa@gmail.com>
2026-05-03 13:50:23 +0200

    Add UserAccount model with unique email index

---
+++ DocVersion.Server/Models/UserAccount.cs
@@ -0,0 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace DocVersion.Server.Models;
+
+[Index(nameof(Email), IsUnique = true)]
+public class UserAccount
+{
+    public long Id { get; set; }
+    public required string Email { get; set; }
+    public required string PasswordHash { get; set; }
+    public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+}
